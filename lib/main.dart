4e2a5a44769ea3a421e2a119d95a6ee9da7c6c93
William Siqueira <wscesar@gmail.com>
2019-05-19 16:43:54 -0300

    removed green theme swatch

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,6 @@ import 'likeWidget.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  
   title: 'Flutter Demo',
-
-  theme: ThemeData(
-    primarySwatch: Colors.green,
-  ),
-
   home: LikeWidget(),
-
 ));
